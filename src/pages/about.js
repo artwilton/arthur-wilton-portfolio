7bd4cc1b0a687eaab382277e14164521a7972873
Arthur <arthur.m.wilton@gmail.com>
2022-08-11 00:20:18 -0400

    created about and work pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>
+                About Page
+            </h1>
+        </main>
+    )
+}
+
+export default AboutPage
