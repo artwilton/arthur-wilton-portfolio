008af0c42d19e3f9fb994771764cdd7d05583e9a
Arthur <arthur.m.wilton@gmail.com>
2022-09-21 15:14:33 -0400

    cleaned up footer tag formatting

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,26 +4,24 @@ import Container from 'react-bootstrap/Container';
 const Footer = (props) => {
   const socialMediaIcons = props.socialMediaIcons
   const socialMediaList = socialMediaIcons.map(({name, link, SVGComp}) =>
-    <li key={name}>
+    <li key={name} className="px-1">
       <a href={link} aria-label={name}>
       <SVGComp/>
       </a>
     </li>
     );
-    
-    return (
-      <Container>
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div class="col-md-4 d-flex align-items-center">
-            <span class="mb-3 mb-md-0 text-muted">© 2022 Arthur Wilton</span>
-          </div>
-      
-          <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            {socialMediaList}
-          </ul>
-        </footer>
-      </Container>
-    )
+
+  return (
+    <Container as="footer" className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div className="col-md-4 d-flex align-items-top">
+        <span className="text-muted">© 2022 Arthur Wilton</span>
+      </div>
+  
+      <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+        {socialMediaList}
+      </ul>
+    </Container>
+  )
 }
 
 export default Footer
