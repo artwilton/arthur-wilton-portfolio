5caedd78bb4bc9a6bd269950acbdcc16c882bd85
Arthur <arthur.m.wilton@gmail.com>
2022-09-21 19:05:44 -0400

    setup imports for homepage media

---
+++ src/media/home/index.js
@@ -0,0 +1,19 @@
+import aboutPhoto from './about-photo-with-dog-4x5.jpg';
+import featuredProject01 from './feautered-project-01.png';
+import landingAboutBG from './landing-about-bg.mp4';
+import landingDefaultBGStill from './landing-default-bg-still.jpg';
+import landingDefaultBG from './landing-default-bg.mp4';
+import workCreativeBG from './work-creative-bg.jpg';
+import workDevBG from './work-dev-bg.jpg';
+import workVideoBG from './work-video-bg.jpg';
+
+export {
+    aboutPhoto,
+    featuredProject01,
+    landingAboutBG,
+    landingDefaultBGStill,
+    landingDefaultBG,
+    workCreativeBG,
+    workDevBG,
+    workVideoBG
+};
