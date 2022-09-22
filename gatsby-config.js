a1613704b725bb9fd647e3911ec92fc6050128b9
Arthur <arthur.m.wilton@gmail.com>
2022-09-22 06:33:48 -0400

    added custom fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,33 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     {
-      resolve: 'gatsby-plugin-react-svg',
+      resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
           include: /social_media_icons/
         }
       }
+    },
+    {
+      resolve: "gatsby-omni-font-loader",
+      options: {
+        enableListener: true,
+        preconnect: ["https://fonts.googleapis.com", "https://fonts.gstatic.com", "https://use.typekit.net", "https://p.typekit.net"],
+        web: [
+          {
+            name: "Montserrat",
+            file: "https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800&display=swap",
+          },
+          {
+            name: "Lato",
+            file: "https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap",
+          },
+          {
+            name: ["europa", "gill-sans-nova"],
+            file: "https://use.typekit.net/phy7oeu.css"
+          }
+        ],
+      },
     }
   ],
   
