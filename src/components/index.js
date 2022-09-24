529d88f58bf379b05bc3bc434607f37415ffd31e
Arthur <arthur.m.wilton@gmail.com>
2022-09-23 20:07:13 -0400

    created index for src/components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Footer from "./footer";
+import Layout from "./layout";
+import Navigation from "./navigation";
+import TitleCard from "./titleCard";
+
+export {
+    Footer,
+    Layout,
+    Navigation,
+    TitleCard
+}
