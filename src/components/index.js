04d318058250cb1f38d27fd7de08dc0c148c63b9
Arthur <arthur.m.wilton@gmail.com>
2022-09-26 05:18:18 -0400

    setting up my work pages

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Footer from "./footer";
 import Layout from "./layout";
 import Navigation from "./navigation";
 import TitleCard from "./titleCard";
+import WorkCard from "./workCard";
 
 export {
     Footer,
     Layout,
     Navigation,
-    TitleCard
+    TitleCard,
+    WorkCard
 }
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,10 +1,28 @@
 import * as React from "react"
-import Layout from "../components/layout"
+import {Layout, WorkCard} from "../components"
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const WorkPage = () => {
     return (
         <Layout>
-            <h1>Work Page</h1>
+            <div className="bg-color--off-white pt-9 pb-7">
+            <Container fluid >
+            <Row>
+                <Col xs="5" className="mx-auto pb-5">
+                    <h1 className=" text-center border-bottom border-dark">My Work</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs="10" className="mx-auto" style={{maxWidth: '1200px'}}>
+                    <WorkCard/>
+                    <WorkCard/>
+                </Col>
+            </Row>
+        </Container>
+        </div>
         </Layout>
     )
 }
