c7b8827132982c9d855ce1996273d294f415fdea
Arthur <arthur.m.wilton@gmail.com>
2022-09-28 00:51:09 -0400

    updated spacing and padding

--- src/components/home/homeWork.js
+++ src/components/home/homeWork.js
@@ -45,20 +45,19 @@ const HomeWork = () => {
         <h1 class="my-work__header border-bottom border-dark align-self-center card-title text-center pb-3">My Work</h1>
             </Col>
         </Row>
-        <Row>
+        <Row className="text-center pb-lg-3">
           <Col>
-          <p className="my-work__lead lead text-center pb-4">I solve technical and <br className="d-md-none" />creative problems using:
-            <br className="d-xl-none mb-3" />
-              <Link
-                style={{display: "inline-block"}}
-                className="my-work__skills-button btn btn-outline-dark btn-lg btn-block ms-lg-3"
-                role="button"
-                block
-                to="/work"
-              >
-                {CycleSkillsText()}
-              </Link>
+          <p style={{display: "inline"}} className="my-work__lead lead text-center moz-smoothed">I solve technical and <br className="d-md-none"/>creative problems using:
+            <br className="d-xl-none"/>
           </p>
+          <Link
+            className="my-work__skills-button btn btn-outline-dark btn-lg btn-block ms-lg-3 mb-3 mt-3 mt-xl-0"
+            role="button"
+            block
+            to="/work"
+          >
+            {CycleSkillsText()}
+          </Link>
           </Col>
         </Row>
         <Row className="pt-2 px-5 g-3 g-lg-4">
