f2a5ae5e65093e44c0a27b8ae7c6442f0d4f09d2
Arthur <arthur.m.wilton@gmail.com>
2022-09-28 23:51:46 -0400

    updated featured project spelling

--- src/media/home/index.js
+++ src/media/home/index.js
@@ -1,5 +1,5 @@
 import aboutPhoto from './about-photo-with-dog-4x5.jpg';
-import featuredProject01 from './feautered-project-01.png';
+import featuredProject01 from './featured-project-01.png';
 import landingAboutBG from './landing-about-bg.mp4';
 import landingDefaultBGStill from './landing-default-bg-still.jpg';
 import landingDefaultBG from './landing-default-bg.mp4';
