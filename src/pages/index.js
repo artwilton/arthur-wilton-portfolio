86f1ab54d846c26d95fcf9f8bee5e01f9c155ac1
Arthur <arthur.m.wilton@gmail.com>
2022-09-29 00:26:34 -0400

    added HomeAbout to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import HomeLanding from "../components/home/homeLanding"
-import { HomeFeatured, HomeWork } from "../components/home"
+import { HomeFeatured, HomeWork, HomeAbout } from "../components/home"
 
 const HomePage = () => {
   return (
@@ -9,6 +9,7 @@ const HomePage = () => {
       <HomeLanding/>
       <HomeWork/>
       <HomeFeatured/>
+      <HomeAbout/>
     </Layout>
   )
 }
