7d80c87bbee6eaccdb414862f5416ca2ab347d4d
Arthur <arthur.m.wilton@gmail.com>
2022-09-29 14:55:10 -0400

    updated homeAbout styling

--- src/components/home/homeAbout.js
+++ src/components/home/homeAbout.js
@@ -9,22 +9,22 @@ import { aboutPhoto } from "../../media/home";
 
 const HomeAbout = () => {
   return (
-    <section id="Section04">
+    <section className="home-about" id="Section04">
       <Container fluid className="py-5 px-5 px-md-0">
         <Row className="pb-5 ">
           <Col md="8" lg="6" className="mx-auto">
-            <h1 class="border-bottom border-dark align-self-center card-title mx-auto text-center pb-3">
+            <h1 class="home-about__header border-bottom border-dark align-self-center card-title mx-auto text-center pb-3">
               Let's Work Together
             </h1>
           </Col>
         </Row>
         <Row>
           <Col md="1" lg="2"></Col>
-          <Col md="5" lg="4" className="text-center text-md-left">
+          <Col md="5" lg="4" className="home-about__description text-center text-md-start">
             <Row>
               <Col sm="12">
                 <p>
-                  <span style={{ fontWeight: 500 }}>
+                  <span className="home-about__description--bold">
                     When I'm not writing code:
                   </span>
                   <br />
@@ -34,7 +34,7 @@ const HomeAbout = () => {
                 </p>
               </Col>
               <Col className="d-none d-xl-block">
-                <p>Have a project you think I might be a good fit for?</p>
+                <p> Have a project you think I might be a good fit for?</p>
                 <Button variant="outline-dark" size="lg" className="px-5">
                   Contact Me
                 </Button>
@@ -43,16 +43,15 @@ const HomeAbout = () => {
           </Col>
           <Col md="5" lg="4">
             <img
-              className="img-fluid mx-auto d-block pt-md-0 pt-3 pl-md-5 pl-0"
+              className="home-about__image img-fluid mx-auto d-block pt-md-0 pt-3 pl-md-5 pl-0"
               src={aboutPhoto}
-              style={{ maxHeight: "520px", "border-radius": "5px" }}
               alt="Arthur holding a cute dog"
             />
           </Col>
           <Col md="1" lg="2"></Col>
         </Row>
         <Row>
-          <Col className="d-xl-none py-5 text-center">
+          <Col className="home-about__description d-xl-none py-5 text-center">
             <p>Have a project you think I might be a good fit for?</p>
             <Button variant="outline-dark" size="lg" className="px-5">
               Contact Me
