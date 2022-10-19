7b1c20bc9eb5384307daf413ac497713ae6a214f
Arthur <arthur.m.wilton@gmail.com>
2022-10-18 23:03:11 -0400

    final nav styling and animation updates

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDebouncedCallback } from 'use-debounce';
+import { useDebouncedCallback } from "use-debounce";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,7 +7,6 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import AdaptiveLink from "./adaptiveLink";
 
 const Navigation = ({ navLinks, navbarBrand, socialMediaIcons }) => {
-
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
   const [offCanvasToggled, setOffCanvasToggled] = useState(false);
@@ -15,26 +14,23 @@ const Navigation = ({ navLinks, navbarBrand, socialMediaIcons }) => {
   const [fadeNav, setFadeNav] = useState(false);
 
   const handleScroll = useDebouncedCallback(
-    // function
     () => {
       const currentScrollPos = window.pageYOffset;
       setVisible(
         (prevScrollPos > currentScrollPos &&
           prevScrollPos - currentScrollPos > 70) ||
-          currentScrollPos < 10
+          currentScrollPos < 20
       );
       setPrevScrollPos(currentScrollPos);
-  
     },
     // delay in ms
-    100
+    80
   );
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll);
-
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [prevScrollPos, visible, handleScroll]);
 
   const handleEnter = () => {
@@ -51,82 +47,103 @@ const Navigation = ({ navLinks, navbarBrand, socialMediaIcons }) => {
   };
   const expand = "md";
 
-  const renderSocialMediaIcons = socialMediaIcons.map(({ name, to, SVGComp }) => (
-    <Navbar.Brand href={to} aria-label={name}>
-      <SVGComp
-        role="img"
-        alt={`${name} Icon`}
-        className={`${
-          offCanvasToggled
-            ? "navbar__social-media-logo--off-canvas mb-4 mx-2 mx-lg-3"
-            : null
-        } navbar__link navbar__social-media-logo d-inline-block align-text-top`}
-      />
-    </Navbar.Brand>
-  ));
+  const renderSocialMediaIcons = socialMediaIcons.map(
+    ({ name, to, SVGComp }) => (
+      <Navbar.Brand href={to} aria-label={name}>
+        <SVGComp
+          role="img"
+          alt={`${name} Icon`}
+          className={`${
+            offCanvasToggled
+              ? "custom-navbar__social-media-logo--off-canvas mb-4 mx-2 mx-lg-3"
+              : ""
+          } custom-navbar__link custom-navbar__social-media-logo d-inline-block align-text-top`}
+        />
+      </Navbar.Brand>
+    )
+  );
 
-  const renderNavLinks = navLinks.map(({name, to}) => (
+  const renderNavLinks = navLinks.map(({ name, to }) => (
     <Nav.Link
-    className={`navbar__link ${
-      offCanvasToggled ? "navbar__link--off-canvas" : null
-    }`}
-    as={AdaptiveLink}
-    to={to}
+      className={`custom-navbar__link ${
+        offCanvasToggled ? "custom-navbar__link--off-canvas" : ""
+      }`}
+      as={AdaptiveLink}
+      to={to}
     >
-    {name}
+      {name}
     </Nav.Link>
   ));
 
+  console.log(prevScrollPos);
   return (
-    <Navbar
-      className={`${offCanvasExited ? null : "navbar--under-logo"} ${visible ? 'navbar--on-screen' : 'navbar--off-screen' }`}
-      variant="dark"
-      expand={expand}
-      fixed="top"
-    >
-      <Container fluid>
-        <Navbar.Brand className="navbar__site-logo ps-4" as={AdaptiveLink} to={navbarBrand.to}>
-          {navbarBrand.logo}
-        </Navbar.Brand>
-        <Navbar.Toggle
-          className="border-0 shadow-none pe-4 me-0"
-          aria-controls={`offcanvasNavbar-expand-${expand}`}
-        />
-        <Navbar.Offcanvas
-          onEnter={handleEnter}
-          onExiting={handleExiting}
-          onExited={handleExited}
-          className="text-bg-dark border-0"
-          id={`offcanvasNavbar-expand-${expand}`}
-          aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-          placement="end"
-        >
-          <Offcanvas.Header
-            className="justify-content-end pt-4"
-            closeButton
-            closeLabel
-            closeVariant="white shadow-none pe-7 pt-3"
+    <>
+      <Navbar.Brand
+        className={`pt-3 ps-4 navbar-dark custom-navbar__link custom-navbar__site-logo ${
+          visible ? "" : "custom-navbar__site-logo--off-screen"
+        }`}
+        as={AdaptiveLink}
+        to={navbarBrand.to}
+      >
+        {navbarBrand.logo}
+      </Navbar.Brand>
+      <Navbar
+        className={`custom-navbar ${
+          visible ? "" : "custom-navbar--off-screen"
+        } ${prevScrollPos ? "" : "custom-navbar--top-of-screen"}`}
+        variant="dark"
+        expand={expand}
+        fixed="top"
+      >
+        <Container fluid>
+          <Navbar.Toggle
+            className="border-0 shadow-none pe-3 ms-auto me-1"
+            aria-controls={`offcanvasNavbar-expand-${expand}`}
           />
-          <Offcanvas.Body
-            className={`
+          <Navbar.Offcanvas
+            onEnter={handleEnter}
+            onExiting={handleExiting}
+            onExited={handleExited}
+            className="text-bg-dark border-0"
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+          >
+            <Offcanvas.Header
+              className="justify-content-end pt-4"
+              closeButton
+              closeLabel
+              closeVariant="white shadow-none pe-7 pt-3"
+            />
+            <Offcanvas.Body
+              className={`
                   ${
                     offCanvasToggled
                       ? "text-center d-flex flex-column align-items-center"
                       : "pt-2"
                   }
-                  ${fadeNav ? "navbar--fade-in" : null}
+                  ${fadeNav ? "custom-navbar--fade-in" : ""}
                 `}
-          >
-            <Nav className={`${offCanvasToggled ? 'my-auto' : 'flex-grow-1 pb-2'} justify-content-end px-5`}>
-              {renderNavLinks}
-            </Nav>
-              <Nav className={`${offCanvasToggled ? 'pt-3 mt-auto flex-row' : 'pt-1 pe-2'}`}>
+            >
+              <Nav
+                className={`${
+                  offCanvasToggled ? "my-auto" : "flex-grow-1 pb-2"
+                } justify-content-end px-5`}
+              >
+                {renderNavLinks}
+              </Nav>
+              <Nav
+                className={`${
+                  offCanvasToggled ? "pt-3 mt-auto flex-row" : "pt-1 pe-2"
+                }`}
+              >
                 {renderSocialMediaIcons}
               </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
