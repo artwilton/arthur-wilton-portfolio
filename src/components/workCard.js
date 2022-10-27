e4cec5776573bd60d5b38d2742d562838bb325d0
Arthur <arthur.m.wilton@gmail.com>
2022-10-27 04:45:19 -0400

    major update to WorkCard

--- src/components/workCard.js
+++ src/components/workCard.js
@@ -4,50 +4,67 @@ import { Link } from "gatsby";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-const WorkCard = ({ name, description, link, demo, imgSource, imgAlt, altLinkTo, altLinkName }) => {
+import { PlayIcon } from "../media/svg_icons";
+
+const WorkCard = ({
+  name,
+  description,
+  link,
+  demo,
+  tags,
+  imgSource,
+  imgAlt,
+  altLinkTo,
+  altLinkName,
+}) => {
   return (
-    <Card className="work-card border-0 shadow-sm">
-      <Row className="g-0">
-        <Col md="5" lg="4">
-          <img className="work-card__image" src={imgSource} alt={imgAlt} />
-        </Col>
-        <Col className="d-flex flex-column text-md-start" md="7" lg="8">
-          <Card.Body>
-            <Card.Title as="h4">{name}</Card.Title>
-            <Card.Text className="work-card__description py-1 pb-sm-2 pb-lg-3">
-              {description}
-            </Card.Text>
-          </Card.Body>
-          <Row className="mb-3 mx-auto mx-md-0 px-1 g-2 g-md-4">
-            <Col className="col-auto mx-auto mx-md-0">
+    <Card className="work-card text-start border-0 shadow-sm h-100">
+      <Card.Img className="work-card__image" src={imgSource} alt={imgAlt} />
+      <Card.Body className="d-flex flex-column border-top border-secondary border-1">
+        <div>
+          {tags?.map((tag) => (
+            <Button
+              className="d-inline-block mb-2 me-1 tag__small border-0 btn-secondary rounded-pill"
+              disabled
+            >
+              {tag}
+            </Button>
+          ))}
+        </div>
+        <Card.Title className="work-card__title mt-1" as="h4">
+          {name}
+        </Card.Title>
+        <Card.Text className="work-card__description">{description}</Card.Text>
+        <Row className="mt-auto">
+          <Col className="col-auto mx-md-0">
+            <Link
+              to={link}
+              className="work-card__button mt-2 btn btn-sm btn-dark shadow-none"
+            >
+              Read More
+            </Link>
+          </Col>
+          <Col className="col-auto mx-md-0">
+            {demo ? (
               <Link
-                to={link}
-                className="work-card__button btn btn-sm btn-dark shadow-none"
+                to={demo}
+                className="work-card__button mt-2 btn btn-outline-dark btn-sm"
               >
-                Read More
+                Demo {<PlayIcon className="mb-1" />}
               </Link>
-            </Col>
-            <Col className="col-auto mx-auto mx-md-0">
-              {demo ? (
-                <Link
-                  to={demo}
-                  className="work-card__button btn btn-outline-dark btn-sm"
-                >
-                  Watch Demo ▶
-                </Link>
-              ) : (
-                <Link
-                  to={altLinkTo}
-                  className="work-card__button btn btn-outline-dark btn-sm"
-                >
-                  {altLinkName}
-                </Link>
-              )}
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+            ) : (
+              <Link
+                to={altLinkTo}
+                className="work-card__button mt-2 btn btn-outline-dark btn-sm"
+              >
+                {altLinkName}
+              </Link>
+            )}
+          </Col>
+        </Row>
+      </Card.Body>
     </Card>
   );
 };
