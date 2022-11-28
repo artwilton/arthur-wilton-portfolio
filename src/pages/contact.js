59ae0bb4ddd9551ff622149980958aa61b41135c
Arthur <arthur.m.wilton@gmail.com>
2022-11-28 18:20:52 -0500

    updated ContactPage styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,33 +14,33 @@ const ContactPage = () => {
     <Layout>
       <Container fluid className="g-0">
         <HeaderWithBGImg title="Contact Me" image={contactBannerImg} />
-        <Row className="bg--light py-5 mx-auto">
+        <Row className="bg--light pt-4 pb-5 py-md-5 mx-auto">
           <Col xs={1} />
-          <Col className="mx-auto" style={{ maxWidth: 850 }}>
-            <p className="lead fw-light">
-              Have a project you think I'd be a good fit for?
+          <Col className="contact-form mx-auto">
+            <p className="contact-form__title--main">
+              Have a role you think I'd be a good fit for?
             </p>
-            <p style={{ fontSize: "1.2rem" }}>
+            <p className="contact-form__title--secondary mb-n1">
               Feel free to connect with me on{" "}
               <a href="https://www.linkedin.com/in/artwilton">LinkedIn</a>, or
               contact me using the form below:
             </p>
-            <Form className=" mt-3 mt-md-5">
-              <Row className="mb-1">
-                <Form.Group as={Col} controlId="formGridName">
+            <Form className="mt-3 mt-md-5">
+              <Row>
+                <Form.Group className="mb-2" as={Col} sm={6} controlId="formGridName">
                   <Form.Label column="lg">Name *</Form.Label>
                   <Form.Control type="text" placeholder="Name" />
                 </Form.Group>
-                <Form.Group as={Col} controlId="formGroupEmail">
+                <Form.Group className="mb-2" as={Col} sm={6} controlId="formGroupEmail">
                   <Form.Label column="lg">Email *</Form.Label>
                   <Form.Control type="email" placeholder="Email" />
                 </Form.Group>
               </Row>
-              <Form.Group className="mb-1" controlId="formGroupSubject">
+              <Form.Group className="mb-2" controlId="formGroupSubject">
                 <Form.Label column="lg">Subject</Form.Label>
                 <Form.Control type="email" placeholder="Subject (optional)" />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formGroupMessage">
+              <Form.Group className="mb-2 mb-md-3" controlId="formGroupMessage">
                 <Form.Label column="lg">Message *</Form.Label>
                 <Form.Control
                   as="textarea"
@@ -48,7 +48,7 @@ const ContactPage = () => {
                   placeholder="Hi Arthur, I'd like to work with you on this project..."
                 />
               </Form.Group>
-              <Button variant="dark" type="submit">
+              <Button className="contact-form__button--submit" variant="dark" type="submit">
                 Send Message
               </Button>
             </Form>
