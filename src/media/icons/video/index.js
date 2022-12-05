378c58b1d27109faef031b4e206973044676ba87
Arthur <arthur.m.wilton@gmail.com>
2022-12-05 17:38:49 -0500

    added video skills icons

---
+++ src/media/icons/video/index.js
@@ -0,0 +1,27 @@
+import AdobeAfterEffectsIcon from "./adobe-after-effects.svg";
+import AdobeMediaEncoderIcon from "./adobe-media-encoder.svg";
+import AdobePhotoshopIcon from "./adobe-photoshop.svg";
+import AdobePremiereProIcon from "./adobe-premiere-pro.svg";
+import AppleCompressorIcon from "./apple-compressor.png";
+import AppleFinalCutProIcon from "./apple-final-cut-pro.png";
+import AvidIcon from "./avid.svg";
+import CatDVIcon from "./catdv.png";
+import DavinciResolveIcon from "./davinci-resolve.png";
+import FFmpegIcon from "./ffmpeg.svg";
+import MochaIcon from "./mocha.png";
+import StorageDNAIcon from "./storage-dna.png";
+
+export {
+  AdobeAfterEffectsIcon,
+  AdobeMediaEncoderIcon,
+  AdobePhotoshopIcon,
+  AdobePremiereProIcon,
+  AppleCompressorIcon,
+  AppleFinalCutProIcon,
+  AvidIcon,
+  CatDVIcon,
+  DavinciResolveIcon,
+  FFmpegIcon,
+  MochaIcon,
+  StorageDNAIcon,
+};
