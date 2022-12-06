9a826763553f77f4f2f794e39e279ad04bb3ab94
Arthur <arthur.m.wilton@gmail.com>
2022-12-05 19:18:16 -0500

    updated video icon exports

--- src/media/icons/video/index.js
+++ src/media/icons/video/index.js
@@ -2,14 +2,14 @@ import AdobeAfterEffectsIcon from "./adobe-after-effects.svg";
 import AdobeMediaEncoderIcon from "./adobe-media-encoder.svg";
 import AdobePhotoshopIcon from "./adobe-photoshop.svg";
 import AdobePremiereProIcon from "./adobe-premiere-pro.svg";
-import AppleCompressorIcon from "./apple-compressor.png";
-import AppleFinalCutProIcon from "./apple-final-cut-pro.png";
+import AppleCompressorIcon from "./apple-compressor.svg";
+import AppleFinalCutProIcon from "./apple-final-cut-pro.svg";
 import AvidIcon from "./avid.svg";
-import CatDVIcon from "./catdv.png";
-import DavinciResolveIcon from "./davinci-resolve.png";
+import CatDVIcon from "./catdv.svg";
+import DavinciResolveIcon from "./davinci-resolve.svg";
 import FFmpegIcon from "./ffmpeg.svg";
-import MochaIcon from "./mocha.png";
-import StorageDNAIcon from "./storage-dna.png";
+import MochaIcon from "./mocha.svg";
+import StorageDNAIcon from "./storage-dna.svg";
 
 export {
   AdobeAfterEffectsIcon,
