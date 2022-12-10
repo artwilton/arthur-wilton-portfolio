b7396b37a6c87ef87dba44b900e3cd0f3d1e75c4
Arthur <arthur.m.wilton@gmail.com>
2022-12-09 23:52:24 -0500

    ContactPage style cleanup

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 const ContactPage = () => {
   return (
