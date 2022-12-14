fb531d833bc10bd12896b31a524cf5654ce9a3b2
Arthur <arthur.m.wilton@gmail.com>
2022-12-14 03:03:51 -0500

    reducing to simple logging example

--- functions/contact.js
+++ functions/contact.js
@@ -1,46 +1,54 @@
 // import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
-import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
+// import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
 
-const DEMO_SECRET_KEY = "1x0000000000000000000000000000000AA";
+// const DEMO_SECRET_KEY = "1x0000000000000000000000000000000AA";
 
-async function handlePost(request) {
-  const body = await request.formData();
-  // Turnstile injects a token in "cf-turnstile-response".
-  const token = body.get("cf-turnstile-response");
-  const ip = request.headers.get("CF-Connecting-IP");
+// async function handlePost(request) {
+//   const body = await request.formData();
+//   // Turnstile injects a token in "cf-turnstile-response".
+//   const token = body.get("cf-turnstile-response");
+//   const ip = request.headers.get("CF-Connecting-IP");
 
-  // Validate the token by calling the "/siteverify" API.
-  let formData = new FormData();
-  formData.append("secret", DEMO_SECRET_KEY);
-  formData.append("response", token);
-  formData.append("remoteip", ip);
+//   // Validate the token by calling the "/siteverify" API.
+//   let formData = new FormData();
+//   formData.append("secret", DEMO_SECRET_KEY);
+//   formData.append("response", token);
+//   formData.append("remoteip", ip);
 
-  const result = await fetch(
-    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-    {
-      body: formData,
-      method: "POST",
-    }
-  );
+//   const result = await fetch(
+//     "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+//     {
+//       body: formData,
+//       method: "POST",
+//     }
+//   );
 
-  const outcome = await result.json();
-  if (!outcome.success) {
-    return new Response(
-      "The provided Turnstile token was not valid! \n" + JSON.stringify(outcome)
-    );
-  }
+//   const outcome = await result.json();
+//   if (!outcome.success) {
+//     return new Response(
+//       "The provided Turnstile token was not valid! \n" + JSON.stringify(outcome)
+//     );
+//   }
 
-  return new Response(
-    "Turnstile token successfuly validated. \n" + JSON.stringify(outcome)
-  );
+//   return new Response(
+//     "Turnstile token successfuly validated. \n" + JSON.stringify(outcome)
+//   );
+// }
+
+// export const onRequest = staticFormsPlugin({
+//   respondWith: async ({ request }) => {
+//     console.log("[LOGGING FROM /contact]", request);
+//     return await handlePost(request);
+//   },
+// });
+
+export async function onRequest(context) {
+  const { request }  = context;
+  console.log(`[LOGGING FROM /contact]: Request came from ${request.url}`);
+
+  return new Response("Hello, world!");
 }
 
-export const onRequest = staticFormsPlugin({
-  respondWith: async ({ request }) => {
-    console.log("[LOGGING FROM /contact]", request);
-    return await handlePost(request);
-  },
-});
 
 // export const onRequest = ({ request, env }) => {
 //   const SECRET_KEY = env.TURNSTILE_SECRET_KEY;
