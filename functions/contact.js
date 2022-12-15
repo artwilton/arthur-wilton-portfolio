793a16f9983734ed10d303a24ca5cac1fd6b45c5
Arthur <arthur.m.wilton@gmail.com>
2022-12-14 19:17:11 -0500

    removed middleware

--- functions/contact.js
+++ functions/contact.js
@@ -1,36 +1,40 @@
-// import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+const DEMO_SECRET_KEY = "1x0000000000000000000000000000000AA";
 
-// export const onRequest = staticFormsPlugin({
-//   respondWith: ({ request }) => {
-//     console.log("[LOGGING FROM /contact]", request);
-//     return await handlePost(request);
-//   },
-// });
+const handlePost = async (request) => {
+  const body = await request.formData();
+  // Turnstile injects a token in "cf-turnstile-response".
+  const token = body.get("cf-turnstile-response");
+  const ip = request.headers.get("CF-Connecting-IP");
 
+  // Validate the token by calling the "/siteverify" API.
+  let formData = new FormData();
+  formData.append("secret", DEMO_SECRET_KEY);
+  formData.append("response", token);
+  formData.append("remoteip", ip);
 
-// export async function onRequest(context) {
-//   const { request }  = context;
-//   console.log(`[LOGGING FROM /contact]: Request came from ${request.url}`);
+  const result = await fetch(
+    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+    {
+      body: formData,
+      method: "POST",
+    }
+  );
 
-//   return await handlePost(request);
-// }
+  const outcome = await result.json();
+  if (!outcome.success) {
+    return new Response(
+      "The provided Turnstile token was not valid! \n" + JSON.stringify(outcome)
+    );
+  }
 
+  console.log("MIDDLEWARE");
 
-// export const onRequest = ({ request, env }) => {
-//   const SECRET_KEY = env.TURNSTILE_SECRET_KEY;
+  return new Response(
+    "Turnstile token successfuly validated. \n" + JSON.stringify(outcome)
+  );
+}
 
-//   mailChannelsPlugin({
-//   personalizations: [
-//     {
-//       to: [{ name: "Contact Form", email: "" }],
-//     },
-//   ],
-//   from: {
-//     name: "Arthur Portfolio",
-//     email: "",
-//   },
-//   respondWith: async () => {
-//     return await handlePost(request, SECRET_KEY);
-//   },
-// });
-// };
+export const onRequestPost = async ({ request, env }) => {
+  console.log(`NODE VERSION: ${env.NODE_VERSION}`);
+  return await handlePost(request);
+};
