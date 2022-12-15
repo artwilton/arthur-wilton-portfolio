320f953b4c7545abcfd06d92dce54f28a3aac1ef
Arthur <arthur.m.wilton@gmail.com>
2022-12-14 19:30:49 -0500

    middleware global test

--- functions/contact.js
+++ functions/contact.js
@@ -1,40 +1,41 @@
-const DEMO_SECRET_KEY = "1x0000000000000000000000000000000AA";
 
-const handlePost = async (request) => {
-  const body = await request.formData();
-  // Turnstile injects a token in "cf-turnstile-response".
-  const token = body.get("cf-turnstile-response");
-  const ip = request.headers.get("CF-Connecting-IP");
+// const DEMO_SECRET_KEY = "1x0000000000000000000000000000000AA";
 
-  // Validate the token by calling the "/siteverify" API.
-  let formData = new FormData();
-  formData.append("secret", DEMO_SECRET_KEY);
-  formData.append("response", token);
-  formData.append("remoteip", ip);
+// const handlePost = async (request) => {
+//   const body = await request.formData();
+//   // Turnstile injects a token in "cf-turnstile-response".
+//   const token = body.get("cf-turnstile-response");
+//   const ip = request.headers.get("CF-Connecting-IP");
 
-  const result = await fetch(
-    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-    {
-      body: formData,
-      method: "POST",
-    }
-  );
+//   // Validate the token by calling the "/siteverify" API.
+//   let formData = new FormData();
+//   formData.append("secret", DEMO_SECRET_KEY);
+//   formData.append("response", token);
+//   formData.append("remoteip", ip);
 
-  const outcome = await result.json();
-  if (!outcome.success) {
-    return new Response(
-      "The provided Turnstile token was not valid! \n" + JSON.stringify(outcome)
-    );
-  }
+//   const result = await fetch(
+//     "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+//     {
+//       body: formData,
+//       method: "POST",
+//     }
+//   );
 
-  console.log("MIDDLEWARE");
+//   const outcome = await result.json();
+//   if (!outcome.success) {
+//     return new Response(
+//       "The provided Turnstile token was not valid! \n" + JSON.stringify(outcome)
+//     );
+//   }
 
-  return new Response(
-    "Turnstile token successfuly validated. \n" + JSON.stringify(outcome)
-  );
-}
+//   console.log("MIDDLEWARE");
 
-export const onRequestPost = async ({ request, env }) => {
-  console.log(`NODE VERSION: ${env.NODE_VERSION}`);
-  return await handlePost(request);
-};
+//   return new Response(
+//     "Turnstile token successfuly validated. \n" + JSON.stringify(outcome)
+//   );
+// }
+
+// export const onRequestPost = async ({ request, env }) => {
+//   console.log(`NODE VERSION: ${env.NODE_VERSION}`);
+//   return await handlePost(request);
+// };
