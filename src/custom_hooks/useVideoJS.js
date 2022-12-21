a3e22b910ba94b66b0a77e57d0ed585689b2fa38
Arthur <arthur.m.wilton@gmail.com>
2022-12-21 16:29:26 -0500

    add useVideoJS hook

---
+++ src/custom_hooks/useVideoJS.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+// Referenced from https://joelhooks.com/usevideojs-a-react-hooks-for-videojs/
+
+export const useVideoJS = (videoJsOptions) => {
+  const videoNode = useRef(null);
+  const player = useRef(null);
+
+  useEffect(() => {
+    player.current = videojs(videoNode.current, videoJsOptions);
+
+    return () => {
+      player.current.dispose();
+    };
+  }, [changedKey]);
+
+  const Video = useCallback(
+    ({ children, ...props }) => {
+      return (
+        <div data-vjs-player key={changedKey}>
+          <video ref={videoNode} className="video-js" {...props}>
+            {children}
+          </video>
+        </div>
+      );
+    },
+    [changedKey]
+  );
+  return { Video, player: player.current };
+};
