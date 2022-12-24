77118d73145f96e12e5f5812a5b828d241e4dcd0
Arthur <arthur.m.wilton@gmail.com>
2022-12-24 14:30:55 -0500

    updated videojs component

---
+++ src/components/videoJS.js
@@ -0,0 +1,53 @@
+import { useEffect, useRef } from 'react'
+import videojs from 'video.js'
+import '../../node_modules/video.js/dist/video-js.min.css'
+
+// referenced from https://videojs.com/guides/react/
+
+export const VideoJS = (props) => {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+  const {options, onReady} = props;
+
+  useEffect(() => {
+
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
+      const videoElement = document.createElement("video-js");
+
+      videoElement.classList.add('vjs-big-play-centered');
+      videoRef.current.appendChild(videoElement);
+
+      const player = playerRef.current = videojs(videoElement, options, () => {
+        videojs.log('player is ready');
+      });
+      onReady && onReady(player);
+
+    // Update existing player on prop change
+    } else {
+      const player = playerRef.current;
+      player.src(options.sources);
+    }
+  }, [options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
+  );
+}
+
+export default VideoJS;
