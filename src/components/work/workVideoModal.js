383dee8a209b2760a417e6ea15f5c29cd24dfc4b
Arthur <arthur.m.wilton@gmail.com>
2022-12-24 14:32:25 -0500

    created workVideoModal which uses videojs

---
+++ src/components/work/workVideoModal.js
@@ -0,0 +1,60 @@
+import { useRef, useState } from "react";
+
+import Modal from "react-bootstrap/Modal";
+import VideoJS from "../videoJS";
+
+const WorkVideoModal = ({ onHide, show, video, title }) => {
+  const [aspectRatio, setAspectRatio] = useState(16/9);
+
+  let style = { "--aspect-ratio": aspectRatio };
+
+  const playerRef = useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    fluid: true,
+    // playbackRates: [0.5, 1, 1.5, 2],
+    responsive: true,
+    sources: [
+      {
+        src: video,
+        type: "video/mp4",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("loadedmetadata", () => {
+      setAspectRatio(player.videoWidth()/player.videoHeight())
+    });
+
+    player.on("dispose", () => {
+      console.log("player will dispose");
+    });
+  };
+
+  console.log(aspectRatio);
+
+  return (
+    <Modal
+    onHide={onHide}
+    show={show}
+    dialogClassName="mx-auto work-video-modal"
+    contentClassName="border-0 bg--light"
+    aria-label={title}
+    centered
+    style={style}
+  >
+    <Modal.Header className="border-0" closeButton></Modal.Header>
+    <Modal.Body className="border-0 mt-n4">
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+    </Modal.Body>
+  </Modal>
+  );
+};
+
+export default WorkVideoModal;
