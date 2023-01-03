1d1aa8e6b92791b12480b08dada3d6392e2658bb
Arthur <arthur.m.wilton@gmail.com>
2023-01-02 21:04:52 -0500

    updated video-js css import

--- src/components/videoJS.js
+++ src/components/videoJS.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import videojs from 'video.js'
-import '../../node_modules/video.js/dist/video-js.min.css'
+import 'video.js/dist/video-js.css'
 
 // referenced from https://videojs.com/guides/react/
 
