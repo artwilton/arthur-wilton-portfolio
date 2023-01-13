bcfb7782e544453f57129c1d19c77b51f3267e4a
Arthur <arthur.m.wilton@gmail.com>
2023-01-13 18:08:56 -0500

    added 'children' prop to headerWithBGImg

--- src/components/headerWithBGImg.js
+++ src/components/headerWithBGImg.js
@@ -1,7 +1,7 @@
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
-const HeaderWithBGImg = ({title, image}) => {
+const HeaderWithBGImg = ({title, image, children}) => {
     return (
         <Row className="header-with-bg-img pt-5 pb-4 py-md-5 g-0">
           <div className="header-with-bg-img__gradient-overlay"></div>
@@ -10,10 +10,11 @@ const HeaderWithBGImg = ({title, image}) => {
             src={image}
             alt={`${title} background`}
           ></img>
-          <Col className="mx-auto mt-4 mb-2 mt-md-6 mb-md-3">
-            <h1 className="header-with-bg-img__title text-center pb-2 pb-md-3">
+          <Col xs="10" md="7" className="text-center mx-auto mt-4 mb-2 mt-md-6 mb-md-4">
+            <h1 className="header-with-bg-img__title">
               {title}
             </h1>
+            {children}
           </Col>
         </Row>
     )
