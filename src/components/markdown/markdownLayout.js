938b8b7e84d75cc745cfc2a4997bc401eb7bdd2f
Arthur <arthur.m.wilton@gmail.com>
2023-01-16 17:54:29 -0500

    integrated InlineVideo into MarkdownLayout

--- src/components/markdown/markdownLayout.js
+++ src/components/markdown/markdownLayout.js
@@ -1,25 +1,24 @@
-import {MDXProvider} from '@mdx-js/react'
-import CodeBlock from './codeBlock'
-import '../../styles/markdown.scss'
- 
+import { MDXProvider } from "@mdx-js/react";
+import { CodeBlock, InlineVideo } from "./";
+import "../../styles/markdown.scss";
+
 const components = {
-    pre: props => <pre {...props} className="markdown__code-block shadow-light"/>,
-    code: ({ children, className }) => {
-  return className ? (
-    <CodeBlock className={className}>{children}</CodeBlock>
-  ) : (
-    <code className="markdown__inline-code">{children}</code>
-  );
-    },
-    p: props => <p {...props} className="markdown__paragraph" />
-  };
- 
+  pre: (props) => (
+    <pre {...props} className="markdown__code-block shadow-light" />
+  ),
+  code: ({ children, className }) => {
+    return className ? (
+      <CodeBlock className={className}>{children}</CodeBlock>
+    ) : (
+      <code className="markdown__inline-code">{children}</code>
+    );
+  },
+  p: (props) => <p {...props} className="markdown__paragraph" />,
+  InlineVideo: (props) => <InlineVideo {...props} className="my-3 my-md-4"/>
+};
+
 const MarkdownLayout = ({ children }) => (
-  <MDXProvider
-    components={components}
-  >
-    {children}
-  </MDXProvider>
-)
+  <MDXProvider components={components}>{children}</MDXProvider>
+);
 
 export default MarkdownLayout;
