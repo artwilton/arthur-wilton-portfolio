2cfd1bc85bee4067215b09e7352b7af3ceaf4758
Arthur <arthur.m.wilton@gmail.com>
2023-01-23 23:49:13 -0500

    implement custom videoJS styles

--- src/components/videoJS.js
+++ src/components/videoJS.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react'
 import videojs from 'video.js'
 import 'video.js/dist/video-js.css'
+import '../styles/vjs-custom.scss'
 
 // referenced from https://videojs.com/guides/react/
 
@@ -15,7 +16,7 @@ const VideoJS = ({ options, onReady }) => {
       // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
       const videoElement = document.createElement("video-js");
 
-      videoElement.classList.add('vjs-big-play-centered');
+      videoElement.classList.add('vjs-custom','vjs-big-play-centered');
       videoRef.current.appendChild(videoElement);
 
       const player = playerRef.current = videojs(videoElement, options, () => {
